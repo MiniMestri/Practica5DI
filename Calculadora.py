ea05ac22cc85b7da692e64b338e90f9a611c8530
MiniMestri <fonsild97@hotmail.com>
2023-11-21 19:27:31 +0100

    Creacion de menu
    He creado un menú desplegable con opciones

--- Calculadora.py
+++ Calculadora.py
@@ -2,7 +2,17 @@ import tkinter as tk
 
 raiz = tk.Tk()
 raiz.title("Aprendiendo Tkinter")
+barramenu=tk.Menu(raiz)
+raiz.config(menu=barramenu)
+calculadora=tk.Menu(barramenu,tearoff=0)
+barramenu.add_cascade(label="Tipos",menu=calculadora)
+
+calculadora.add_command(label="Básica")
+calculadora.add_command(label="Avanzada")
+calculadora.add_command(label="Progreso")
 
 raiz.geometry("350x500")
 
 raiz.mainloop()
+
+
