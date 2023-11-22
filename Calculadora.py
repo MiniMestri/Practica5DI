b8fe7d6ccdfb1c0e07ce3894decc28c8d2e2e2a4
MiniMestri <fonsild97@hotmail.com>
2023-11-22 18:51:08 +0100

    Funcionamiento calculadora basica
    He desarrollado la parte de calculadora basica, es decir, el funcionamiento de los botones con sus respectivos valores numericos y sus acciones en el programa como la de hacer funcional la calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,29 +1,50 @@
 import tkinter as tk
 
 def grafica():
-    tk.Button(raiz,text="1",width=5,height=2).grid(row=1, column=0, padx=5,pady=10)
-    tk.Button(raiz,text="2",width=5,height=2).grid(row=1, column=1, padx=5,pady=10)
-    tk.Button(raiz,text="3",width=5,height=2).grid(row=1, column=2, padx=5,pady=10)
-    tk.Button(raiz,text="4",width=5,height=2).grid(row=2, column=0, padx=5,pady=10)
-    tk.Button(raiz,text="5",width=5,height=2).grid(row=2, column=1, padx=5,pady=10)
-    tk.Button(raiz,text="6",width=5,height=2).grid(row=2, column=2, padx=5,pady=10)
-    tk.Button(raiz,text="7",width=5,height=2).grid(row=3, column=0, padx=5,pady=10)
-    tk.Button(raiz,text="8",width=5,height=2).grid(row=3, column=1, padx=5,pady=10)
-    tk.Button(raiz,text="9",width=5,height=2).grid(row=3, column=2, padx=5,pady=10)
-    tk.Button(raiz,text="0",width=10,height=2).grid(row=4, column=1, padx=5,pady=10)
+    tk.Button(raiz,text="1",width=5,height=2,command=lambda: valor(1)).grid(row=1, column=0, padx=5,pady=10)
+    tk.Button(raiz,text="2",width=5,height=2,command=lambda: valor(2)).grid(row=1, column=1, padx=5,pady=10)
+    tk.Button(raiz,text="3",width=5,height=2,command=lambda: valor(3)).grid(row=1, column=2, padx=5,pady=10)
+    tk.Button(raiz,text="4",width=5,height=2,command=lambda: valor(4)).grid(row=2, column=0, padx=5,pady=10)
+    tk.Button(raiz,text="5",width=5,height=2,command=lambda: valor(5)).grid(row=2, column=1, padx=5,pady=10)
+    tk.Button(raiz,text="6",width=5,height=2,command=lambda: valor(6)).grid(row=2, column=2, padx=5,pady=10)
+    tk.Button(raiz,text="7",width=5,height=2,command=lambda: valor(7)).grid(row=3, column=0, padx=5,pady=10)
+    tk.Button(raiz,text="8",width=5,height=2,command=lambda: valor(8)).grid(row=3, column=1, padx=5,pady=10)
+    tk.Button(raiz,text="9",width=5,height=2,command=lambda: valor(9)).grid(row=3, column=2, padx=5,pady=10)
+    tk.Button(raiz,text="0",width=10,height=2,command=lambda: valor(0)).grid(row=4, column=1, padx=5,pady=10)
+
+def operaciones():
+    tk.Button(raiz,text="+",width=5,height=2,command=lambda: valor("+")).grid(row=5, column=0, padx=5,pady=10)
+    tk.Button(raiz,text="-",width=5,height=2,command=lambda: valor("-")).grid(row=5, column=1, padx=5,pady=10)
+    tk.Button(raiz,text="*",width=5,height=2,command=lambda: valor("*")).grid(row=5, column=2, padx=5,pady=10)
+    tk.Button(raiz,text=":",width=5,height=2,command=lambda: valor("/")).grid(row=5, column=3, padx=5,pady=10)
+
+def valor(num):
+    entrada.insert(tk.END,str(num))
     
-def basica():
-    tk.Entry(raiz).grid(row=0, column=1, padx=5,pady=10)
-    grafica()
-    
+def calcular():
+    resultado=eval(entrada.get())
+    entrada.delete(0,tk.END)
+    entrada.insert(tk.END,str(resultado))
+
+def borrar():
+    longitud=len(entrada.get())-1
+    entrada.delete(longitud,tk.END)
+        
 raiz = tk.Tk()
 raiz.title("Aprendiendo Tkinter")
+
 barramenu=tk.Menu(raiz)
 raiz.config(menu=barramenu)
 calculadora=tk.Menu(barramenu,tearoff=0)
 barramenu.add_cascade(label="Tipos",menu=calculadora)
 
-calculadora.add_command(label="Básica", command=basica)
+calculadora.add_command(label="Básica", command=operaciones)
+entrada=tk.Entry(raiz)
+entrada.grid(row=0, column=1, padx=5,pady=10)
+tk.Button(raiz,text="=",width=20,height=4,command=calcular).grid(row=6, column=1, padx=5,pady=10)
+tk.Button(raiz,text="BORRAR",width=10,height=4,command=borrar).grid(row=6, column=2, padx=5,pady=10)
+grafica()
+
 calculadora.add_command(label="Avanzada")
 calculadora.add_command(label="Progreso")
 
